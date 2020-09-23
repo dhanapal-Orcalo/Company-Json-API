bbeae4d8cc075f8a25d0c42a57c3e6b54c5c0b38
Dhanapal <dhanapalk@valitor.com>
2020-09-23 16:45:16 +0100

    Change RJS

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Example1 from '.Example1';
+import Example2 from '.Example2';
+import Example3 from '.Example3';
+ 
+class App extends Component {
+    render() {
+        return (
+            div className=App
+				Example1Example1
+				Example2Example2
+				Example3Example3
+            div
+        );
+    }
+}
+ 
+export default App;
